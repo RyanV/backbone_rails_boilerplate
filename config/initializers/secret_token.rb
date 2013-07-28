59bd1da188f8f671ed6c609cbbbb30234d2b0a76
Ryan Venegas <ryan@milyoni.com>
2013-07-27 23:47:48 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackboneRailsBoilerplate::Application.config.secret_key_base = '61f788be8c407f314b78c0020394b7e25bfa89a22e7822f5675c36b4d039ad39d8386f01d08c169bc068f60989e12338cd01af79c2611ab37aca508af3bc8c44'
